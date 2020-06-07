ff288349bdfb6f6fd23131edd29456e5e8e6750b
amananku26 <amankumaranku@gmail.com>
2020-06-07 23:28:36 +0530

    bug fix

--- Sprint-2/script.js
+++ Sprint-2/script.js
@@ -1,29 +1,82 @@
-var btn;
+//function to get values, store them into an array and display them one below other. 
 
-function add() {
-    var insert = document.getElementById('insert')
-    var div = document.createElement('div')
-    var p = document.createElement('p')
-    p.textContent = insert
-    var btn = document.createElement('button')
-    button.textContent = "remove"
-    btn.addEventListener('click', removeItem)
-    div.append(p, btn)
+arr = []
+arr1 = []
 
-    renderDom(insert)
+function IncomeAmountEntry() {
+    // get value of income from the first input box
+    var showIncome = Number(document.getElementById('incomeAmountValues').value)
+    var showIncomeSource = document.getElementById('income1').value;
+    // console.log(showIncome)
+    arr.push(showIncome)
+        // console.log(showIncomeSource)
+        // arr.push(incomeAmountValues)
+        // var amount = ''
+    var AmountValue = document.createElement('div')
+
+    // AmountValue.setAttribute("class", "amtdiv")
+
+    var sourceValue = document.createElement('div')
+
+    // sourceValue.setAttribute("class", "txtdiv")
+    AmountValue.innerHTML = showIncome
+    sourceValue.innerHTML = showIncomeSource
+
+
+    // for (var i = 0; i < arr.length; i++) {
+    //     amount = amount + arr[i]
+    // }IncomeSource
+    // var x = document.getElementById('IncomeAmount').innerHTML = showIncome
+    // var y = document.getElementById('IncomeSource').innerHTML = showIncomeSource
+    document.getElementById("IncomeAmount").append(AmountValue)
+    document.getElementById("IncomeSource").append(sourceValue)
+    console.log(arr)
 
 }
 
-function removeItem(div) {
-    event.target.parentElement.remove()
+function IncomeSpentEntry() {
+    var showSpent = Number(document.getElementById('spentAmount').value)
+    var showSpentSource = document.getElementById('spent').value;
+    arr1.push(showSpent)
+        // console.log(p)
+        //     var q = document.getElementById('spentAmountShow').innerHTML = p
+        //     var y = document.getElementById('spentSource').innerHTML = showSpentSource
+    var SpentValue = document.createElement('div')
+    var IncomesourceValue = document.createElement('div')
+
+    SpentValue.innerHTML = showSpent
+    IncomesourceValue.innerHTML = showSpentSource
+
+
+    document.getElementById("spentAmountShow").append(SpentValue)
+    document.getElementById("spentSource").append(IncomesourceValue)
+    console.log(arr1)
+
 }
 
-function renderDom(element) {
-    var target = document.getElementById('res1')
-    target.append(element)
-}
+function total() {
+    var sum = 0
 
-window.addEventListener('load', function() {
-    btn = document.getElementById('submit')
-    btn.addEventListener('click', add)
-})
+    for (var i = 0; i < arr.length; i++) {
+        sum = sum + arr[i]
+    }
+    console.log(sum)
+
+    var count = 0
+    for (var j = 0; j < arr1.length; j++) {
+        count = count + arr[j]
+    }
+    console.log(count)
+    var showTotal = sum - count
+    console.log(showTotal)
+
+    if (showTotal <= 0) {
+        var out = "YOUR SPENT IS MORE THEN INCOME"
+    } else if (showTotal > 0 && showTotal < 5000) {
+        out = "BALANCE IS LOW:.. " + showTotal
+    } else if (showTotal >= 5000) {
+        out = "WOW! YOUR BALANCE IS " + showTotal
+    }
+    document.getElementById('total-amount').innerHTML = out
+
+}
